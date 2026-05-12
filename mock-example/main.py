edeba8c0170188a69994a84be70a6d5fd5457710
hllwwwrld <arkoah072@gmail.com>
2026-05-12 21:51:22 +0500

    часть 2

--- mock-example/main.py
+++ mock-example/main.py
@@ -2,13 +2,13 @@ import uvicorn
 import fastapi
 
 from app.api.business.service import BusinessService
-from app.pkg.tables.prepared_response import prepared_scenario_table
+from app.pkg.tables.prepared_response import prepared_response_table
 from app.pkg.tables.user import user_table
 
 if __name__ == "__main__":
     app: fastapi.FastAPI = fastapi.FastAPI()
     business_service = BusinessService(
-        server=app, user_table=user_table, prepared_scenario_table=prepared_scenario_table
+        server=app, user_table=user_table, prepared_scenario_table=prepared_response_table
     )
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
